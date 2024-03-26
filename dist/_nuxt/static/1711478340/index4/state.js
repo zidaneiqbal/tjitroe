0c9a3e1de7167fa4316147c2db492d0a41ebf6a4
Muhammad Zidane Iqbal <raja.zidaneiqbal@gmail.com>
2024-03-27 03:49:55 +0700

    [Update]

--- dist/_nuxt/static/1711478340/index4/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Ftjitroe\u002F_nuxt\u002Fstatic\u002F1711478340",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},state:{searchPopupStatus:b},serverRendered:b,routePath:a,config:{_app:{basePath:"\u002Ftjitroe\u002F",assetsPath:"\u002Ftjitroe\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Findex4",false));
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,13 @@
 
 export default {
-  mode: "universal",
-
   target: "static",
   router: {
     base: "/tjitroe/",
+  },
+
+  app: {
+    baseURL: "/tjitroe/", // baseURL: '/<repository>/'
+    buildAssetsDir: "assets", // don't use "_" at the begining of the folder name to avoids nojkill conflict
   },
 
   generate: {
@@ -30,41 +33,47 @@ export default {
         rel: "apple-touch-icon",
         sizes: "180x180",
         type: "image/x-icon",
-        href: "/assets/images/favicons/apple-touch-icon.png",
+        href: "/tjitroe/assets/images/favicons/apple-touch-icon.png",
       },
       {
         rel: "icon",
         sizes: "32x32",
         type: "image/png",
-        href: "/assets/images/favicons/favicon-32x32.png",
+        href: "/tjitroe/assets/images/favicons/favicon-32x32.png",
       },
       {
         rel: "icon",
         sizes: "16x16",
         type: "image/png",
-        href: "/assets/images/favicons/favicon-16x16.png",
+        href: "/tjitroe/assets/images/favicons/favicon-16x16.png",
       },
       {
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Barlow:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600&display=swap",
       },
-      { rel: "stylesheet", href: "/assets/css/bootstrap.min.css" },
-      { rel: "stylesheet", href: "/assets/css/bootstrap-datepicker.min.css" },
-      { rel: "stylesheet", href: "/assets/css/vegas.min.css" },
-      { rel: "stylesheet", href: "/assets/css/animate.min.css" },
+      { rel: "stylesheet", href: "/tjitroe/assets/css/bootstrap.min.css" },
       {
         rel: "stylesheet",
-        href: "/assets/plugins/glightbox/glightbox.min.css",
+        href: "/tjitroe/assets/css/bootstrap-datepicker.min.css",
       },
-      { rel: "stylesheet", href: "/assets/css/fontawesome-all.min.css" },
-      { rel: "stylesheet", href: "/assets/css/agrikol_iconl.css" },
-      { rel: "stylesheet", href: "/assets/css/style.css" },
-      { rel: "stylesheet", href: "/assets/css/responsive.css" },
+      { rel: "stylesheet", href: "/tjitroe/assets/css/vegas.min.css" },
+      { rel: "stylesheet", href: "/tjitroe/assets/css/animate.min.css" },
+      {
+        rel: "stylesheet",
+        href: "/tjitroe/assets/plugins/glightbox/glightbox.min.css",
+      },
+      {
+        rel: "stylesheet",
+        href: "/tjitroe/assets/css/fontawesome-all.min.css",
+      },
+      { rel: "stylesheet", href: "/tjitroe/assets/css/agrikol_iconl.css" },
+      { rel: "stylesheet", href: "/tjitroe/assets/css/style.css" },
+      { rel: "stylesheet", href: "/tjitroe/assets/css/responsive.css" },
     ],
 
     script: [
-      { src: "/assets/plugins/glightbox/glightbox.min.js", body: true },
-      { src: "/assets/plugins/accordion/accordion.min.js", body: true },
+      { src: "/tjitroe/assets/plugins/glightbox/glightbox.min.js", body: true },
+      { src: "/tjitroe/assets/plugins/accordion/accordion.min.js", body: true },
     ],
   },
   /*
