74a675571028eecb092b5db95937d61e121fa680
Muhammad Zidane Iqbal <raja.zidaneiqbal@gmail.com>
2024-03-27 04:07:50 +0700

    Updates

---
+++ _nuxt/static/1711487266/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fcheckout","\u002F","\u002Fabout","\u002Fnews","\u002Fcontact","\u002Ffarmers","\u002Fnews_detail","\u002Fcart","\u002Fgallery","\u002Fprojects_detail","\u002Fproduct","\u002Fproduct-detail","\u002Fprojects","\u002Fservice","\u002Fwhy_choose_us","\u002Fservice-detail"]})
